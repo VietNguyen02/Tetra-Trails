52075f3e7e047af0892e69453e4a66a998041e3f
Viet Nguyen <VietTNguyen2002@gmail.com>
2024-04-27 13:30:04 -0400

    Updated state machine and animation tree fully. Animation player, tree, and scripts are in sync with custom state machine

--- Scripts/CharacterStateMachine.gd
+++ Scripts/CharacterStateMachine.gd
@@ -3,6 +3,8 @@ extends Node
 class_name CharacterStateMachine
 
 @export var current_state : State
+@export var character : CharacterBody2D
+@export var animation_tree : AnimationTree
 
 var states : Array[State]
 
@@ -12,8 +14,25 @@ func _ready():
 			states.append(child)
 			
 			# Set the states up with what they need to function
+			child.character = character
+			child.playback = animation_tree["parameters/playback"]
 		else:
 			push_warning("Child " + child.name + " is not a State for CharacterStateMachine.")
 
+func _physics_process(delta):
+	if (current_state.next_state != null):
+		switch_states(current_state.next_state)
+	current_state.state_process(delta)
+	
 func check_if_can_move():
 	return current_state.can_move
+	
+func switch_states(new_state : State):
+	if (current_state != null):
+		current_state.on_exit()
+		current_state.next_state = null
+	current_state = new_state
+	current_state.on_enter()
+	
+func _input(event: InputEvent):
+	current_state.state_input(event)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 200.0
-@export var jump_velocity = -125.0
-@export var double_jump_velocity = -100.0
 
 # Animation Child Object reference
 @onready var sprite : Sprite2D = $Sprite2D
@@ -12,10 +9,7 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped: bool= false
-var animation_locked: bool = false
 var direction: Vector2 = Vector2.ZERO
-var was_in_air: bool = false
 
 func _ready():
 	animation_tree.active = true
@@ -24,24 +18,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
-	else:
-		has_double_jumped = false
-		
-		if was_in_air == true:
-			land()
-		
-		was_in_air = false
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		# Single jump off floor
-		if is_on_floor():
-			jump()
-		# Double jump off first jump
-		elif not has_double_jumped:
-			double_jump()
-			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -64,22 +40,3 @@ func update_facing_direction():
 	elif direction.x < 0:
 		sprite.flip_h = true
 		
-func jump():
-	velocity.y = jump_velocity
-	# animated_sprite.play("jump_start")
-	animation_locked = true
-
-func double_jump():
-	velocity.y = double_jump_velocity
-	# animated_sprite.play("jump_double")
-	animation_locked = true
-	has_double_jumped = true
-
-func land():
-	animation_locked = true
-	# animated_sprite.play("jump_end")
-
-
-#func _on_animated_sprite_2d_animation_finished():
-	#if (["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
-		#animation_locked = false
